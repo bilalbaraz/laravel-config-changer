0948b32196f9082616b3e756bc3d70bc7d457b25
Bilal Baraz <bilalbaraz@windowslive.com>
2020-04-07 12:41:02 +0300

    Add travisci and unit test folder

---
+++ tests/BaseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class BaseTest
+ * @package Tests\Unit
+ */
+class BaseTest extends TestCase
+{
+    /**
+     * @test
+     */
+    function it_should_assert_true()
+    {
+        $this->assertTrue(true);
+    }
+}
